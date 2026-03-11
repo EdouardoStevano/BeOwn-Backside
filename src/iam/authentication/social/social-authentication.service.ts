55e5eb2436f62d310750151f130f9bdc5eaf9e7a
ravikazaha <andrianantenainacharlotvincent@gmail.com>
2026-03-11 15:05:08 +0300

    IMPROVE(US-1): remove unused import in authService

--- src/iam/authentication/social/social-authentication.service.ts
+++ src/iam/authentication/social/social-authentication.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TokenService } from 'src/iam/token/token.service';
 import { User } from 'src/users/entities/user.entity';
