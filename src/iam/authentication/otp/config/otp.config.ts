cd21a0c65a0d6a2fce4882f6b40d60fd702f6228
ravikazaha <andrianantenainacharlotvincent@gmail.com>
2026-03-30 20:37:05 +0300

    remove iam and email

--- src/iam/authentication/otp/config/otp.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('otp', () => {
-  return {
-    otpTtl: Number(process.env.OTP_TTL || 300),
-    maxAttemps: Number(process.env.OTP_MAX_ATTEMPS || 3),
-  };
-});
--- src/iam/authentication/social/guards/google.guards.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Social } from '../constant/social';
-
-@Injectable()
-export class GoogleAuthGuard extends AuthGuard(Social.GOOGLE) {}
