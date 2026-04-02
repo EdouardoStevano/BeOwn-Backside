adce781514ffe9b26a19d2efc429aabfa7ed2855
ravikazaha <andrianantenainacharlotvincent@gmail.com>
2026-04-02 15:07:08 +0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/applications/users.module';
 import { IamModule } from './iam/iam.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      prort: Number(process.env.REDIS_PORT),
+    }),
+
     ConfigModule.forRoot({ envFilePath: '.env' }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -16,6 +25,22 @@ import { IamModule } from './iam/iam.module';
       database: process.env.DATABASE_DB || 'postgres',
       autoLoadEntities: true,
       synchronize: true,
+    }),
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: process.env.MAIL_HOST || 'smtp.gmail.com',
+          port: Number(process.env.MAIL_PORT) || 587,
+          secure: false,
+          auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_USER_PASSWORD,
+          },
+        },
+        defaults: {
+          from: `"Beown" <${process.env.MAIL_FROM}>`,
+        },
+      }),
     }),
     UsersModule,
     IamModule,
